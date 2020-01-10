ed55d6dc883e8d0592239f4ef7c67613cd53db7d
911user <911user@911users-MacBook-Pro.local>
2020-01-10 15:04:30 -0600

    fix naming folder

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+
+
+ <style> 
+    #footer{
+        background-color: #2B4988;
+        width: 100%;
+        height: 10%;
+           }
+    #sub {
+        background-color: rgb(50, 6, 155);
+        width: 100%;
+        height: 10%;
+           }
+  </style>
+hello
+
+<div id="footer"></div>
+<div id="sub"></div>
+
+
---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+
+
+ <style> 
+    #header{
+        background-color: #d0d3d9;
+        width: 100%;
+        height: 10%;
+           }
+    #sub {
+        background-color: #ffffff;
+        width: 100%;
+        height: 10%;
+           }
+  </style>
+
+
+<div id="header"></div>
+<div id="sub"></div>
+
